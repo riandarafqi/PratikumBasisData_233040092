d758e1e08afd0b30aa42368c38417267b82111cf
riandarafqi <riandarafqi321@gmail.com>
2024-10-10 00:42:03 +0700

    mengerjakan latihan 1&2

--- Latihan_Pratikum_BasisData_1/nomor_1.sql
+++ Latihan_Pratikum_BasisData_1/nomor_1.sql
@@ -1,12 +1,12 @@
-SELECT 
-    empid,
-    firstname,
-    lastname,
-    titleofcourtesy,
-    CASE 
-        WHEN titleofcourtesy IN ('Ms.', 'Mrs.') THEN 'Female'
-        WHEN titleofcourtesy = 'Mr.' THEN 'Male'
-        ELSE 'Unknown'
-    END AS gender
+SELECT
+	empid,
+	firstname,
+	lastname,
+	titleofcourtesy,
+CASE
+	WHEN titleofcourtesy IN ('Ms.', 'Mrs.') THEN 'Female'
+	WHEN titleofcourtesy = 'Mr.' THEN 'Male'
+	ELSE 'Unknown'
+END AS gender
 FROM
 	HR.Employees;
--- Latihan_Pratikum_BasisData_1/nomor_2.sql
+++ Latihan_Pratikum_BasisData_1/nomor_2.sql
@@ -1,14 +1,8 @@
-SELECT 
-  c.custid, 
-  COUNT(o.orderid) AS numorders, 
-  SUM(od.qty) AS totalqty
-FROM 
-  Sales.Customers c
-  INNER JOIN Sales.Orders o ON c.custid = o.custid
-  INNER JOIN Sales.OrderDetails od ON o.orderid = od.orderid
-WHERE 
-  c.custid IN (32, 36, 43, 45, 48, 55, 65, 71, 75, 77, 78, 82, 89)
-GROUP BY 
-  c.custid
-ORDER BY 
-  c.custid;
+SELECT c.custid,
+	COUNT(o.orderid) AS "Total Pesanan",
+	SUM(od.qty) AS "Total kuantitas"
+FROM Sales.Customers c
+JOIN Sales.Orders o ON o.custid = c.custid
+JOIN Sales.OrderDetails od ON od.orderid = o.orderid
+WHERE c.country = 'USA'
+GROUP BY c.custid;
