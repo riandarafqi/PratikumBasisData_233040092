546a706dd34b1dfaf2ea8964b4e893c38cea595b
riandarafqi <riandarafqi321@gmail.com>
2024-10-16 17:42:10 +0700

    mengerjakan latihan pertemuan4

---
+++ Pertemuan4/Latihan2/Soal3.sql
@@ -0,0 +1,3 @@
+SELECT COUNT(*) AS 'Jumlah Total Mahasiswa Informatika'
+FROM mahasiswa
+WHERE jurusan = 'Teknik Informatika';
---
+++ Pertemuan4/Latihan2/Soal4.sql
@@ -0,0 +1,2 @@
+SELECT TOP 5 * FROM dpp_mahasiswa
+ORDER BY jumlah_pembayaran DESC;
